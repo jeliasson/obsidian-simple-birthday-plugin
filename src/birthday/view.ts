364bcb0c6968a8dd4e13ca0c53bc56f4d08495c3
Johan Eliasson <jeliasson@users.noreply.github.com>
2025-07-16 20:30:35 +0200

    fix: style and class naming (#3)
    * fix: style
    
    * fix: rename sample classes
    
    * chore: version bump

--- src/birthday/view.ts
+++ src/birthday/view.ts
@@ -1,18 +1,18 @@
 import { ItemView, WorkspaceLeaf, setIcon } from 'obsidian';
-import type MyPlugin from '../main';
+import type SimpleBirthdayPlugin from '../main';
 import { extractBirthdaysFromFiles, sortBirthdays } from './utils';
 import type { BirthdayEntry } from '../types/birthday';
 
 export const BIRTHDAY_VIEW_TYPE = 'birthday-sidebar-view';
 
 export class View extends ItemView {
-  readonly plugin: MyPlugin;
+  readonly plugin: SimpleBirthdayPlugin;
   static instance: View | null = null;
   private _todayRow: HTMLTableRowElement | null = null;
 
   birthdayListEl: HTMLElement | null = null;
 
-  constructor(leaf: WorkspaceLeaf, plugin: MyPlugin) {
+  constructor(leaf: WorkspaceLeaf, plugin: SimpleBirthdayPlugin) {
     super(leaf);
 	
     this.plugin = plugin;
@@ -114,7 +114,7 @@ export class View extends ItemView {
           monthHeader.classList.add('birthday-table__month-header--current');
         }
 
-        monthRow.style.height = '10px';
+        monthRow.classList.add('birthday-table__month-row');
         lastMonth = month;
       }
       
